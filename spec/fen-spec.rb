9fb5f448322dbc9342c96daf323b168a94e0e7fd
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-08 17:20:44 +0530

    Add fen tests

--- spec/fen-spec.rb
+++ spec/fen-spec.rb
@@ -5,5 +5,56 @@ require_relative '../lib/fen'
 
 describe Fen do
   subject(:fen) { described_class.new }
-  let(:piece) { instance_double(Piece) }
+
+  describe '#to_pieces' do
+    context 'when default fen given' do
+      fen_code = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+
+      it 'returns an array of board pieces with 32 nils in correct places' do
+        pieces_array = [nil] * 32
+        result = fen.to_pieces(fen_code)
+        expect(result[16..47]).to eq(pieces_array)
+      end
+
+      it 'returns an array with first element as white rook' do
+        result = fen.to_pieces(fen_code)[0]
+        expect(result.name).to eq('r')
+        expect(result.color).to eq('white')
+      end
+
+      it 'returns an array with last element as black rook' do
+        result = fen.to_pieces(fen_code)[-1]
+        expect(result.name).to eq('R')
+        expect(result.color).to eq('black')
+      end
+    end
+
+    context 'when custom fen given' do
+      fen_code = 'rnb1kbnr/ppp2ppp/3ppq2/8/8/BPN5/P1PPPPPP/R2QKBNR w KQkq - 0 4'
+
+      it 'returns an array of board pieces with 18 nils in correct places' do
+        pieces_array = [nil] * 18
+        result = fen.to_pieces(fen_code)
+        expect(result[22..39]).to eq(pieces_array)
+      end
+
+      it 'returns an array with 20th element as white pawn' do
+        result = fen.to_pieces(fen_code)[19]
+        expect(result.name).to eq('p')
+        expect(result.color).to eq('white')
+      end
+
+      it 'returns an array with first element as white rook' do
+        result = fen.to_pieces(fen_code)[0]
+        expect(result.name).to eq('r')
+        expect(result.color).to eq('white')
+      end
+
+      it 'returns an array with last element as black rook' do
+        result = fen.to_pieces(fen_code)[-1]
+        expect(result.name).to eq('R')
+        expect(result.color).to eq('black')
+      end
+    end
+  end
 end

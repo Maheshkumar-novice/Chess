08ac2490cc124dd47cd0c5c7a83f3b45f3635fe5
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-10 12:31:09 +0530

    Add tests for Bishop - #classify_moves

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -44,4 +44,82 @@ describe 'Bishop' do
       end
     end
   end
+
+  describe 'Board#classify_moves' do
+    subject(:board) { Board.new }
+    let(:result) { board.classify_moves(cell, board.create_moves(cell)) }
+
+    context 'with default fen board' do
+      context 'when moves of white bishop c1 are classified' do
+        let(:cell) { :c1 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty]
+          expected_result = []
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures]
+          expected_result = []
+          expect(captures).to eq(expected_result)
+        end
+      end
+
+      context 'when moves of black bishop f8 are classified' do
+        let(:cell) { :f8 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty]
+          expected_result = []
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures]
+          expected_result = []
+          expect(captures).to eq(expected_result)
+        end
+      end
+    end
+
+    context 'with custom fen board' do
+      subject(:board) { Board.new(fen) }
+      let(:fen) do
+        'rnk2bnr/p1p2ppp/1p1qp1N1/2Bp1b2/3P4/1PP5/P1Q1PPPP/RN2KB1R b KQ - 1 9'
+      end
+
+      context 'when moves of white bishop f5 are classified' do
+        let(:cell) { :f5 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty].sort
+          expected_result = %i[g4 h3].sort
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures].sort
+          expected_result = %i[e6 g6].sort
+          expect(captures).to eq(expected_result)
+        end
+      end
+
+      context 'when moves of black bishop c5 are classified' do
+        let(:cell) { :c5 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty].sort
+          expected_result = %i[d4 e3 b4 a3].sort
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures].sort
+          expected_result = %i[f2 b6].sort
+          expect(captures).to eq(expected_result)
+        end
+      end
+    end
+  end
 end

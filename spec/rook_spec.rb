f8310cf909ca7d0c755aef9784c6f1f019e9613a
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-10 12:22:37 +0530

    Add tests for Rook - #classify_moves

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -44,4 +44,82 @@ describe 'Rook' do
       end
     end
   end
+
+  describe 'Board#classify_moves' do
+    subject(:board) { Board.new }
+    let(:result) { board.classify_moves(cell, board.create_moves(cell)) }
+
+    context 'with default fen board' do
+      context 'when moves of white rook h1 are classified' do
+        let(:cell) { :h1 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty]
+          expected_result = []
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures]
+          expected_result = []
+          expect(captures).to eq(expected_result)
+        end
+      end
+
+      context 'when moves of black rook a8 are classified' do
+        let(:cell) { :a8 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty]
+          expected_result = []
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures]
+          expected_result = []
+          expect(captures).to eq(expected_result)
+        end
+      end
+    end
+
+    context 'with cusom fen board' do
+      subject(:board) { Board.new(fen) }
+      let(:fen) do
+        'rnb1kbn1/2p1ppp1/1p6/pPP2r1p/P2pPQ2/3R2qP/3P1PP1/1NB1KBNR b Kq - 4 11'
+      end
+
+      context 'when moves of white rook e3 are classified' do
+        let(:cell) { :e3 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty].sort
+          expected_result = %i[d3 c3 f3 g3 h3].sort
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures].sort
+          expected_result = %i[b3 e4].sort
+          expect(captures).to eq(expected_result)
+        end
+      end
+
+      context 'when moves of black rook c5 are classified' do
+        let(:cell) { :c5 }
+
+        it 'returns all the empty moves' do
+          empty = result[:empty].sort
+          expected_result = %i[b5 d5 e5 c6].sort
+          expect(empty).to eq(expected_result)
+        end
+
+        it 'returns all the captures' do
+          captures = result[:captures].sort
+          expected_result = %i[f5 c4].sort
+          expect(captures).to eq(expected_result)
+        end
+      end
+    end
+  end
 end

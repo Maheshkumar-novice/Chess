72ed35b55e6aa20d72bb25be244574c3edd9552e
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-14 08:39:58 +0530

    Refactor board helper

--- lib/modules/board-helper.rb
+++ lib/modules/board-helper.rb
@@ -7,18 +7,16 @@ require_relative '../cell'
 module BoardHelper
   private
 
-  def reject_moves_of_same_color(moves, color)
-    moves.reject do |move|
-      @board[move].piece&.color == color
+  def create_board
+    board = {}
+    @rows.each do |row|
+      @columns.each do |column|
+        cell_marker = create_cell_marker(row, column)
+        board[cell_marker] = create_cell(row, column)
+        board[cell_marker].piece.current_cell = cell_marker if board[cell_marker].piece
+      end
     end
-  end
-
-  def king_position(king_color)
-    @board.each do |marker, cell|
-      color = cell.piece&.color
-      name = cell.piece&.name.to_s.downcase
-      return marker if color == king_color && name == 'k'
-    end
+    board
   end
 
   def create_cell_marker(row, column)
@@ -28,74 +26,37 @@ module BoardHelper
   def create_cell(row, column)
     cell = Cell.new
     cell.piece = @pieces.shift
-    create_row_connections(row, column, cell)
-    create_column_connections(row, column, cell)
-    create_diagonal_connections(row, column, cell)
+    cell.create_connections(row, column)
     cell
   end
 
-  def create_row_connections(row, column, cell)
-    cell.row_right = create_row_right_connection(row, column)
-    cell.row_left = create_row_left_connection(row, column)
+  def reject_moves_of_same_color_destination(moves, color)
+    moves.reject do |move|
+      @board[move].piece&.color == color
+    end
   end
 
-  def create_row_right_connection(row, column)
-    return if column == 'h'
-
-    (column.next + row.to_s).to_sym
+  def reject_moves_that_keep_own_king_in_check(source, destinations, color)
+    destinations.reject do |destination|
+      move_leads_to_check?(source, destination, color)
+    end
   end
 
-  def create_row_left_connection(row, column)
-    return if column == 'a'
-
-    ((column.ord - 1).chr + row.to_s).to_sym
+  def move_leads_to_check?(source, destination, color)
+    previous_source_piece = @board[source].piece
+    previous_destination_piece = @board[destination].piece
+    make_move(source, destination)
+    king_in_check = @board[find_king_position(color)].piece.in_check?(@board)
+    @board[source].piece = previous_source_piece
+    @board[destination].piece = previous_destination_piece
+    king_in_check
   end
 
-  def create_column_connections(row, column, cell)
-    cell.column_above = create_column_above_connection(row, column)
-    cell.column_below = create_column_below_connection(row, column)
-  end
-
-  def create_column_above_connection(row, column)
-    return if row == 8
-
-    (column + (row + 1).to_s).to_sym
-  end
-
-  def create_column_below_connection(row, column)
-    return if row == 1
-
-    (column + (row - 1).to_s).to_sym
-  end
-
-  def create_diagonal_connections(row, column, cell)
-    cell.top_left_diagonal = create_top_left_connection(row, column)
-    cell.top_right_diagonal = create_top_right_connection(row, column)
-    cell.bottom_right_diagonal = create_bottom_right_connection(row, column)
-    cell.bottom_left_diagonal = create_bottom_left_connection(row, column)
-  end
-
-  def create_top_left_connection(row, column)
-    return if row == 8 || column == 'a'
-
-    ((column.ord - 1).chr + (row + 1).to_s).to_sym
-  end
-
-  def create_top_right_connection(row, column)
-    return if row == 8 || column == 'h'
-
-    (column.next + (row + 1).to_s).to_sym
-  end
-
-  def create_bottom_right_connection(row, column)
-    return if row == 1 || column == 'h'
-
-    (column.next + (row - 1).to_s).to_sym
-  end
-
-  def create_bottom_left_connection(row, column)
-    return if row == 1 || column == 'a'
-
-    ((column.ord - 1).chr + (row - 1).to_s).to_sym
+  def find_king_position(king_color)
+    @board.each do |marker, cell|
+      color = cell.piece&.color
+      name = cell.piece&.name.to_s.downcase
+      return marker if color == king_color && name == 'k'
+    end
   end
 end

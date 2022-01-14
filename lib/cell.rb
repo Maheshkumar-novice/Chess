6fac53778fb8560ba085b995f47172e61ab61384
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-14 08:39:00 +0530

    Move connection related methods to cell class

--- lib/cell.rb
+++ lib/cell.rb
@@ -12,4 +12,77 @@ class Cell
                 :top_right_diagonal,
                 :bottom_right_diagonal,
                 :bottom_left_diagonal
+
+  def create_connections(row, column)
+    create_row_connections(row, column)
+    create_column_connections(row, column)
+    create_diagonal_connections(row, column)
+  end
+
+  private
+
+  def create_row_connections(row, column)
+    self.row_right = create_row_right_connection(row, column)
+    self.row_left = create_row_left_connection(row, column)
+  end
+
+  def create_column_connections(row, column)
+    self.column_above = create_column_above_connection(row, column)
+    self.column_below = create_column_below_connection(row, column)
+  end
+
+  def create_diagonal_connections(row, column)
+    self.top_left_diagonal = create_top_left_connection(row, column)
+    self.top_right_diagonal = create_top_right_connection(row, column)
+    self.bottom_right_diagonal = create_bottom_right_connection(row, column)
+    self.bottom_left_diagonal = create_bottom_left_connection(row, column)
+  end
+
+  def create_column_above_connection(row, column)
+    return if row == 8
+
+    (column + (row + 1).to_s).to_sym
+  end
+
+  def create_row_right_connection(row, column)
+    return if column == 'h'
+
+    (column.next + row.to_s).to_sym
+  end
+
+  def create_row_left_connection(row, column)
+    return if column == 'a'
+
+    ((column.ord - 1).chr + row.to_s).to_sym
+  end
+
+  def create_column_below_connection(row, column)
+    return if row == 1
+
+    (column + (row - 1).to_s).to_sym
+  end
+
+  def create_top_left_connection(row, column)
+    return if row == 8 || column == 'a'
+
+    ((column.ord - 1).chr + (row + 1).to_s).to_sym
+  end
+
+  def create_top_right_connection(row, column)
+    return if row == 8 || column == 'h'
+
+    (column.next + (row + 1).to_s).to_sym
+  end
+
+  def create_bottom_right_connection(row, column)
+    return if row == 1 || column == 'h'
+
+    (column.next + (row - 1).to_s).to_sym
+  end
+
+  def create_bottom_left_connection(row, column)
+    return if row == 1 || column == 'a'
+
+    ((column.ord - 1).chr + (row - 1).to_s).to_sym
+  end
 end

821c9e69dfdf3ca23dd0eff8ad97cd5221a34df3
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-16 10:53:26 +0530

    Update rook tests to reflect new board format

--- spec/pieces/rook_spec.rb
+++ spec/pieces/rook_spec.rb
@@ -31,19 +31,19 @@ describe Rook do
         'rnbqk2r/ppppppb1/5n2/6p1/P6p/3PP2P/1PPNBPPR/R1BQK1N1 b Qkq - 2 7'
       end
 
-      it 'returns all the moves of white rook from h1' do
-        cell = :h1
+      it 'returns all the moves of white rook from a1' do
+        cell = :a1
         rook = board[cell].piece
         result = rook.create_moves(board).sort
-        expected_result = %i[g1 f1 h2 h3 h4].sort
+        expected_result = %i[a2 a3 a4 b1 c1].sort
         expect(result).to eq(expected_result)
       end
 
-      it 'returns all the moves of black rook from a8' do
-        cell = :a8
+      it 'returns all the moves of black rook from h8' do
+        cell = :h8
         rook = board[cell].piece
         result = rook.create_moves(board).sort
-        expected_result = %i[a7 a6 a5 a4 b8 c8 d8].sort
+        expected_result = %i[h7 h6 h5 h4 g8 f8 e8].sort
         expect(result).to eq(expected_result)
       end
     end
@@ -78,12 +78,12 @@ describe Rook do
         'rnb1kbn1/2p1ppp1/1p6/pPP2r1p/P2pPQ2/3R2qP/3P1PP1/1NB1KBNR b Kq - 4 11'
       end
 
-      it 'returns all the classified moves of white rook e3' do
-        cell = :e3
+      it 'returns all the classified moves of white rook d3' do
+        cell = :d3
         rook = board[cell].piece
         moves = rook.create_moves(board)
         result = rook.classify_moves(moves, board)
-        expected_result = { captures: %i[b3 e4], empty: %i[f3 g3 h3 d3 c3] }
+        expected_result = { captures: %i[g3 d4], empty: %i[a3 b3 c3 e3 f3] }
         result[:empty] = result[:empty].sort
         result[:captures] = result[:captures].sort
         expected_result[:empty] = expected_result[:empty].sort
@@ -91,12 +91,12 @@ describe Rook do
         expect(result).to eq(expected_result)
       end
 
-      it 'returns all the classified moves of black rook c5' do
-        cell = :c5
+      it 'returns all the classified moves of black rook f5' do
+        cell = :f5
         rook = board[cell].piece
         moves = rook.create_moves(board)
         result = rook.classify_moves(moves, board)
-        expected_result = { captures: %i[f5 c4], empty: %i[d5 e5 b5 c6] }
+        expected_result = { captures: %i[c5 f4], empty: %i[f6 g5 d5 e5] }
         result[:empty] = result[:empty].sort
         result[:captures] = result[:captures].sort
         expected_result[:empty] = expected_result[:empty].sort

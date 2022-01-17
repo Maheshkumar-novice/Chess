8b7a3e3149782cc6cb6c29653af026dee4c4da59
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-17 14:00:48 +0530

    Add #create_name for Bot

--- lib/players/bot.rb
+++ lib/players/bot.rb
@@ -6,11 +6,16 @@ require_relative './player'
 # Bot Player
 class Bot < Player
   COLUMNS = ('a'..'h').to_a
+  NAMES = %w[nick fury rick morty].freeze
 
   def input
     row = random_row
     column = random_column
     (column + row)
+  end
+
+  def create_name
+    @name = NAMES.sample
   end
 
   private

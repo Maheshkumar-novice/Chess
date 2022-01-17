0907549d56cd2e035c310778a5db2d1759142a9d
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-17 14:01:01 +0530

    Add #create_name for Human

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -8,4 +8,15 @@ class Human < Player
   def input
     gets.chomp
   end
+
+  def create_name
+    @name = name_input until valid_name?(@name.to_s)
+  end
+
+  private
+
+  def name_input
+    print 'Enter Your Name > '
+    @name = gets.chomp
+  end
 end

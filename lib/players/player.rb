b91a3cb3c85efb0557e47c66cb5a8e16542c0d20
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-17 12:27:31 +0530

    Create player class

---
+++ lib/players/player.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Player Class
+class Player
+  attr_accessor :name, :color
+
+  MAX_NAME_LENGTH = 15
+
+  def initialize
+    @name = nil
+    @color = nil
+  end
+
+  def input; end
+
+  def valid_name?(name)
+    name.match?(/^[\w+\s]{1,#{MAX_NAME_LENGTH}}$/)
+  end
+end

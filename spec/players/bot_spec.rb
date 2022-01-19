6459464beca78aa9b13df3b5ef564ada05400dbb
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 11:33:16 +0530

    Remove bot tests

--- spec/players/bot_spec.rb
+++ spec/players/bot_spec.rb
@@ -5,19 +5,4 @@ require_relative '../../lib/players/bot'
 
 describe Bot do
   subject(:bot) { described_class.new }
-
-  describe '#input' do
-    it 'returns a string' do
-      result = bot.input.is_a?(String)
-      expect(result).to eq true
-    end
-  end
-
-  describe '#create_name' do
-    it 'returns a string of size between 1 and 15' do
-      bot.create_name
-      result = bot.name.size.between?(1, 15)
-      expect(result).to eq true
-    end
-  end
 end

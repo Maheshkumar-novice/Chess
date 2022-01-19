495c329adc96e7929d5b78d32e9342ac96f05e51
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 15:09:08 +0530

    Add updated method tests for bot

--- spec/players/bot_spec.rb
+++ spec/players/bot_spec.rb
@@ -5,4 +5,22 @@ require_relative '../../lib/players/bot'
 
 describe Bot do
   subject(:bot) { described_class.new }
+
+  describe '#make_choice' do
+    let(:cell_choice_maker) { bot.instance_variable_get(:@cell_choice_maker) }
+
+    it 'sends message bot_choice to cell_choice_maker once' do
+      expect(cell_choice_maker).to receive(:bot_choice).once
+      bot.make_choice
+    end
+  end
+
+  describe '#create_name' do
+    let(:name_creator) { bot.instance_variable_get(:@name_creator) }
+
+    it 'sends message bot_name to name_creator' do
+      expect(name_creator).to receive(:bot_name)
+      bot.create_name
+    end
+  end
 end

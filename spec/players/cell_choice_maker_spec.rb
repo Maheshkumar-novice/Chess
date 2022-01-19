eabe55965f3327871cfc2df5c2077ed5ec232ec5
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 15:10:06 +0530

    Add tests for cell_choic_maker

---
+++ spec/players/cell_choice_maker_spec.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../lib/players/cell-choice-maker'
+
+describe CellChoiceMaker do
+  subject(:cell_choice_maker) { described_class.new }
+
+  describe '#bot_choice' do
+    it 'returns a string' do
+      name = cell_choice_maker.bot_choice
+      result = name.is_a?(String)
+      expect(result).to eq true
+    end
+  end
+
+  describe '#human_choice' do
+    before do
+      allow(cell_choice_maker).to receive(:print)
+      allow(cell_choice_maker).to receive(:gets).and_return('a name')
+    end
+
+    it 'returns a string' do
+      name = cell_choice_maker.human_choice
+      result = name.is_a?(String)
+      expect(result).to eq true
+    end
+  end
+end

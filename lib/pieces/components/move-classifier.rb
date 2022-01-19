01b51fd872b0268fd2c6f6ba0cac4816a476087c
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 18:22:34 +0530

    Add move-classifier to pieces components

---
+++ lib/pieces/components/move-classifier.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+# Move Classifier
+class MoveClassifier
+  def classify_moves(color, moves, board)
+    enemy_color = color == 'white' ? 'black' : 'white'
+
+    result = {
+      captures: [],
+      empty: []
+    }
+    moves.each_with_object(result) do |move, classified|
+      classified[:empty] << move if board[move].piece.nil?
+      classified[:captures] << move if board[move].piece&.color == enemy_color
+    end
+  end
+end

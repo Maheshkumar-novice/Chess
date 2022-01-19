19eb93b50c977a10fb57de38284dc883885a2fbe
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 18:23:38 +0530

    Update knight to use composition

--- lib/pieces/knight.rb
+++ lib/pieces/knight.rb
@@ -1,11 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative '../piece'
+require_relative './components/move-generator'
+require_relative './components/move-classifier'
 
 # Knight
-class Knight < Piece
+class Knight
+  attr_accessor :name, :color, :current_cell
+
+  def initialize(move_generator: MoveGenerator.new, move_classifier: MoveClassifier.new)
+    @name = nil
+    @color = nil
+    @current_cell = nil
+    @move_generator = move_generator
+    @move_classifier = move_classifier
+  end
+
   def create_moves(board)
-    knight_moves(@current_cell, board)
+    @move_generator.knight_moves(@current_cell, board)
+  end
+
+  def classify_moves(moves, board)
+    @move_classifier.classify_moves(color, moves, board)
   end
 end

3153a208e7b7242f396003344a63e6ab4baf3126
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 15:10:20 +0530

    Add tests for cell_choice_validator.rb

---
+++ spec/players/cell_choice_validator.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../lib/players/cell-choice-validator'
+
+describe CellChoiceValidator do
+  subject(:cell_choice_validator) { described_class.new }
+
+  describe '#valid?' do
+    context 'when the given choice is an empty string' do
+      it 'returns false' do
+        choice = ''
+        result = cell_choice_validator.valid?(choice)
+        expect(result).to eq false
+      end
+    end
+
+    context 'when the given choice is of length greater than 2' do
+      it 'returns false' do
+        choice = 'a21'
+        result = cell_choice_validator.valid?(choice)
+        expect(result).to eq false
+      end
+    end
+
+    context 'when the given choice is valid' do
+      it 'returns true' do
+        choice = 'b1'
+        result = cell_choice_validator.valid?(choice)
+        expect(result).to eq true
+      end
+    end
+  end
+end

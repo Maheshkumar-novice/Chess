4557bc23eb0b502cab659dcf4423021046f91257
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 15:11:21 +0530

    Add max_name_length instance variable - NameValidator

--- lib/players/name-validator.rb
+++ lib/players/name-validator.rb
@@ -3,7 +3,11 @@
 
 # Name Validator
 class NameValidator
+  def initialize
+    @max_name_length = 25
+  end
+
   def valid?(name)
-    name.match?(/^[\w+\s]{1,25}$/)
+    name.match?(/^[\w+\s]{1,#{@max_name_length}}$/)
   end
 end

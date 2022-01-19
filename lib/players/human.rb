a8f9db25774775719950cc6d0e80773b8830e304
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-19 11:28:14 +0530

    Update human to use composition

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,22 +1,34 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative './player'
+require_relative './name-creator'
+require_relative './name-validator'
+require_relative './cell-choice-maker'
+require_relative './cell-choice-validator'
 
 # Human Player
-class Human < Player
-  def input
-    gets.chomp
+class Human
+  attr_accessor :name, :color
+
+  def initialize(name_creator: NameCreator.new,
+                 cell_choice_maker: CellChoiceMaker.new,
+                 name_validator: NameValidator.new,
+                 cell_choice_validator: CellChoiceValidator.new)
+    @name = nil
+    @color = nil
+    @name_creator = name_creator
+    @cell_choice_maker = cell_choice_maker
+    @name_validator = name_validator
+    @cell_choice_validator = cell_choice_validator
+  end
+
+  def make_choice
+    cell_choice = @cell_choice_maker.human_choice
+    cell_choice = @cell_choice_maker.human_choice until @cell_choice_validator.valid?(cell_choice)
   end
 
   def create_name
-    @name = name_input until valid_name?(@name.to_s)
-  end
-
-  private
-
-  def name_input
-    print 'Enter Your Name > '
-    @name = gets.chomp
+    @name = @name_creator.human_name
+    @name = @name_creator.human_name until @name_validator.valid?(@name)
   end
 end

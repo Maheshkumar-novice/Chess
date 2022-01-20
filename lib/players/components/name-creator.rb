2b90c2b9941731964174963762ad0073da1390b4
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-20 20:14:46 +0530

    Add display module to name-creator

--- lib/players/components/name-creator.rb
+++ lib/players/components/name-creator.rb
@@ -1,14 +1,18 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative '../../utils/display'
+
 # Name Creator
 class NameCreator
+  include Display
+
   def bot_name
     %w[nick fury rick morty].sample
   end
 
   def human_name
-    print 'Enter Your Name > '
+    print_prompt('Enter Your Name > ')
     gets.chomp
   end
 end

7f8d2748249fcfbb04917bf95b830e77225a7504
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-20 20:16:38 +0530

    Add display module to human

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -5,9 +5,12 @@ require_relative './components/name-creator'
 require_relative './components/name-validator'
 require_relative './components/cell-choice-maker'
 require_relative './components/cell-choice-validator'
+require_relative '../utils/display'
 
 # Human Player
 class Human
+  include Display
+
   attr_accessor :name, :color
 
   def initialize(name_creator: NameCreator.new,
@@ -24,12 +27,21 @@ class Human
 
   def make_choice
     cell_choice = @cell_choice_maker.human_choice
-    cell_choice = @cell_choice_maker.human_choice until @cell_choice_validator.valid?(cell_choice)
+    until @cell_choice_validator.valid?(cell_choice)
+      print_error('Enter a valid cell coordinate!', ending: "\n")
+      cell_choice = @cell_choice_maker.human_choice
+    end
     cell_choice
   end
 
   def create_name
     @name = @name_creator.human_name
-    @name = @name_creator.human_name until @name_validator.valid?(@name)
+    until @name_validator.valid?(@name)
+      print_error(
+        "Enter a valid name! (Min. Length 1, Max. Length #{@name_validator.max_name_length}, No special characters)",
+        ending: "\n"
+      )
+      @name = @name_creator.human_name
+    end
   end
 end

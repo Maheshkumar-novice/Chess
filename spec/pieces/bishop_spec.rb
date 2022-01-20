789801d681be09497d2015002a63240170281a53
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-20 21:27:55 +0530

    Update bishop-tests fen dependency

--- spec/pieces/bishop_spec.rb
+++ spec/pieces/bishop_spec.rb
@@ -2,11 +2,20 @@
 # frozen_string_literal: true
 
 require_relative '../../lib/board'
+require_relative '../../lib/fen'
 
 describe Bishop do
+  let(:fen) { Fen.new }
+  let(:default_fen) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' }
+
+  before do
+    fen.process(fen_code)
+  end
+
   describe '#create_moves' do
     context 'with default fen board' do
-      let(:board) { Board.new.board }
+      let(:fen_code) { default_fen }
+      let(:board) { Board.new(fen.pieces, fen.meta_data).board }
 
       it 'returns all the moves of white bishop from f1' do
         cell = :f1
@@ -26,10 +35,8 @@ describe Bishop do
     end
 
     context 'with custom fen board' do
-      let(:board) { Board.new(fen).board }
-      let(:fen) do
-        'rnbqk2r/ppppppb1/5n2/6p1/P6p/3PP2P/1PPNBPPR/R1BQK1N1 b Qkq - 2 7'
-      end
+      let(:fen_code) { 'rnbqk2r/ppppppb1/5n2/6p1/P6p/3PP2P/1PPNBPPR/R1BQK1N1 b Qkq - 2 7' }
+      let(:board) { Board.new(fen.pieces, fen.meta_data).board }
 
       it 'returns all the moves of white bishop from e2' do
         cell = :e2
@@ -51,7 +58,8 @@ describe Bishop do
 
   describe '#classify_moves' do
     context 'with default fen board' do
-      let(:board) { Board.new.board }
+      let(:fen_code) { default_fen }
+      let(:board) { Board.new(fen.pieces, fen.meta_data).board }
 
       it 'returns all the classified moves of white bishop c1' do
         cell = :c1
@@ -73,10 +81,8 @@ describe Bishop do
     end
 
     context 'with custom fen board' do
-      let(:board) { Board.new(fen).board }
-      let(:fen) do
-        'rnk2bnr/p1p2ppp/1p1qp1N1/2Bp1b2/3P4/1PP5/P1Q1PPPP/RN2KB1R b KQ - 1 9'
-      end
+      let(:fen_code) { 'rnk2bnr/p1p2ppp/1p1qp1N1/2Bp1b2/3P4/1PP5/P1Q1PPPP/RN2KB1R b KQ - 1 9' }
+      let(:board) { Board.new(fen.pieces, fen.meta_data).board }
 
       it 'returns all the classified moves of white bishop c5' do
         cell = :c5

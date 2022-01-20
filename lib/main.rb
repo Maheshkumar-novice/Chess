5046fbc43dc37659da2a865d5c9b843d02a2fadb
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-20 20:23:12 +0530

    Create main.rb

---
+++ lib/main.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative './game'
+require_relative './fen'
+require_relative './board'
+require_relative './players/players-creator'
+
+fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1'
+fen_processor = Fen.new
+fen_processor.process(fen)
+pieces = fen_processor.pieces
+current_color = fen_processor.current_color
+meta_data = fen_processor.meta_data
+
+board = Board.new(pieces, meta_data)
+
+players = PlayersCreator.new.create_players
+players.rotate if current_color == 'black'
+
+game = Game.new(board, players, current_color)
+game.play

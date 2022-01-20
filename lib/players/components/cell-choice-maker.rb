1e59eeae9d049a0b5d13533ae8794ff4f877806d
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-20 20:13:38 +0530

    Add display module - cell-choice-maker

--- lib/players/components/cell-choice-maker.rb
+++ lib/players/components/cell-choice-maker.rb
@@ -1,8 +1,12 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative '../../utils/display'
+
 # Cell Choice Maker
 class CellChoiceMaker
+  include Display
+
   def initialize
     @rows = ('1'..'8').to_a
     @columns = ('a'..'h').to_a
@@ -13,7 +17,7 @@ class CellChoiceMaker
   end
 
   def human_choice
-    print 'Enter The Cell Coordinate (i.e. a1, b7, c3) > '
+    print_prompt('Enter The Cell Coordinate (i.e. a1) > ')
     gets.chomp
   end
 end

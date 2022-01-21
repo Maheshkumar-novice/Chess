aaf7d7e86904d87e0c7035d9d691b9ef615361a8
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-21 16:07:41 +0530

    Include game utils to game

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,12 @@
 # frozen_string_literal: true
 
 require_relative './utils/display'
+require_relative './utils/game-utils'
 
 # Game
 class Game
   include Display
+  include GameUtils
 
   def initialize(board, players, current_color)
     @board = board
@@ -110,32 +112,5 @@ class Game
 
   def switch_players
     @current_player, @other_player = @other_player, @current_player
-  end
-
-  def sleep_if_bot
-    sleep(1) if @current_player.is_a?(Bot)
-  end
-
-  def print_board
-    system('clear')
-    @board.print_board(@source_choice, @moves[:empty], @moves[:captures])
-  end
-
-  def print_check_status
-    print_info("Is your king in check? #{accent(@board.king_in_check?(@current_color).to_s)}", ending: "\n")
-  end
-
-  def print_current_player_info
-    print_info("#{accent(@current_player.name)}'s move (#{accent(@current_color)}):", ending: "\n")
-  end
-
-  def print_error_if_human(str)
-    print_error_if(str, condition: @current_player.is_a?(Human),
-                        ending: "\n")
-  end
-
-  def print_info_if_human(str)
-    print_info_if(str, condition: @current_player.is_a?(Human),
-                       ending: "\n")
   end
 end

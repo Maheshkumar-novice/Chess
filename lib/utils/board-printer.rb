e557c1603a677e8f0a5cd8aa9dc2f9c1de1da8ef
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-22 16:48:28 +0530

    Refactor board-printer to be more readable

--- lib/utils/board-printer.rb
+++ lib/utils/board-printer.rb
@@ -5,49 +5,81 @@ require_relative './color'
 
 # Board Printer
 class BoardPrinter
+  LIGHT_BG = :bg_gray
+  DARK_BG = :bg_cyan
+
+  def initialize
+    @board = nil
+    @source = nil
+    @empty = []
+    @captures = []
+    @default_bg_color = LIGHT_BG
+    @row_no = 8
+  end
+
   def print_board(board, source = nil, empty = [], captures = [])
+    instance_variables_set(board, source, empty, captures)
     print_column_info
-    print_board_data(board, source, empty, captures)
+    print_board_data
     print_column_info
   end
 
   private
 
-  def print_column_info
-    column_info = ('a'..'h').to_a.map { |value| "  #{value}".bold.gray }.join.prepend('  ')
-    puts <<~COLUMN
-      #{column_info}
-    COLUMN
+  def instance_variables_set(board, source, empty, captures)
+    @board = board
+    @source = source
+    @empty = empty
+    @captures = captures
   end
 
-  def print_board_data(board, source, empty, captures)
-    default_bg_color = :bg_cyan
-    row = 9
+  def print_column_info
+    puts ('a'..'h').to_a.map { |value| row_column_color("  #{value}") }.join.prepend('  ')
+  end
 
-    board.each do |key, cell|
-      print " #{row -= 1} ".bold.gray if key.match?(/a/)
-      default_bg_color = switch_bg(default_bg_color)
-      print_cell(cell, get_bg_color(key, source, empty, captures, default_bg_color))
+  def print_board_data
+    @board.each do |key, cell|
+      print_row_no(condition: key.match?(/a/))
+      print_cell(cell, get_bg_color(key))
+      switch_bg
       next unless key.match?(/h/)
 
-      print " #{row} \n".bold.gray
-      default_bg_color = switch_bg(default_bg_color)
+      print_row_no(ending: "\n")
+      switch_bg
+      decrement_row_no
     end
+    reset
   end
 
-  def switch_bg(bg_color)
-    bg_color == :bg_cyan ? :bg_gray : :bg_cyan
+  def print_row_no(condition: true, ending: '')
+    print row_column_color(" #{@row_no} " + ending) if condition
+  end
+
+  def row_column_color(str)
+    str.bold.gray
   end
 
   def print_cell(cell, bg_color)
     print " #{cell} ".send(bg_color).black
   end
 
-  def get_bg_color(key, source, empty, captures, default_bg_color)
-    return :bg_magenta if source == key
-    return :bg_red if captures.include?(key)
-    return :bg_green if empty.include?(key)
+  def get_bg_color(key)
+    return :bg_magenta if @source == key
+    return :bg_red if @captures.include?(key)
+    return :bg_green if @empty.include?(key)
 
-    default_bg_color
+    @default_bg_color
+  end
+
+  def switch_bg
+    @default_bg_color = @default_bg_color == DARK_BG ? LIGHT_BG : DARK_BG
+  end
+
+  def decrement_row_no
+    @row_no -= 1
+  end
+
+  def reset
+    initialize
   end
 end

eb61619922f2322ed89b0c23b98ba5f097e360f8
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-22 21:51:37 +0530

    Update fen to use piece-creator

--- lib/fen.rb
+++ lib/fen.rb
@@ -1,21 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative './pieces/rook'
-require_relative './pieces/knight'
-require_relative './pieces/bishop'
-require_relative './pieces/king'
-require_relative './pieces/queen'
-require_relative './pieces/pawn'
+require_relative './utils/piece-creator'
 
 # FEN Operations
 class Fen
   attr_reader :pieces, :current_color, :meta_data
 
-  def initialize
+  def initialize(piece_creator: PieceCreator.new)
     @pieces = nil
     @current_color = nil
     @meta_data = nil
+    @piece_creator = piece_creator
   end
 
   def process(fen)
@@ -34,56 +30,7 @@ class Fen
 
   def to_pieces(rows)
     rows[-1] = rows[-1].split[0]
-    create_pieces(rows)
-  end
-
-  def create_pieces(rows)
-    pieces = []
-    rows.each do |row|
-      row.chars.each do |value|
-        pieces += make_piece(value)
-      end
-    end
-    pieces
-  end
-
-  def make_piece(value)
-    pieces = []
-    if value.match?(/^[1-8]{1}$/)
-      pieces += create_nil_pieces(value.to_i)
-    else
-      pieces << create_valid_piece(value)
-    end
-    pieces
-  end
-
-  def create_nil_pieces(value)
-    [nil] * value
-  end
-
-  def create_valid_piece(value)
-    piece_symbol = value.downcase.to_sym
-    piece = piece(piece_symbol)
-    piece.name = value
-    piece.color = piece_color(value)
-    piece
-  end
-
-  def piece(piece_symbol)
-    {
-      r: Rook.new,
-      b: Bishop.new,
-      n: Knight.new,
-      k: King.new,
-      q: Queen.new,
-      p: Pawn.new
-    }[piece_symbol]
-  end
-
-  def piece_color(value)
-    return 'white' if value.match?(/^[[:upper:]]{1}$/)
-
-    'black'
+    @piece_creator.create_pieces(rows)
   end
 
   def parse_color(value)

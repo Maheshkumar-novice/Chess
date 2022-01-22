1bdd96a77e4be655bb59543ddf8b2beb1baf6745
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-22 14:32:58 +0530

    Improve readability - move-classifier

--- lib/pieces/components/move-classifier.rb
+++ lib/pieces/components/move-classifier.rb
@@ -6,12 +6,9 @@ class MoveClassifier
   def classify_moves(color, moves, board)
     enemy_color = color == 'white' ? 'black' : 'white'
 
-    result = {
-      captures: [],
-      empty: []
-    }
+    result = { empty: [], captures: [] }
     moves.each_with_object(result) do |move, classified|
-      classified[:empty] << move unless board[move].occupied?
+      classified[:empty] << move if board[move].empty?
       classified[:captures] << move if board[move].piece_color == enemy_color
     end
   end

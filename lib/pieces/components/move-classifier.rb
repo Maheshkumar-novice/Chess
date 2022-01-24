314e2b964ac7ea82b68e284982491ae1284cbb7e
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-24 18:31:27 +0530

    Refactor move-classifier methods

--- lib/pieces/components/move-classifier.rb
+++ lib/pieces/components/move-classifier.rb
@@ -3,36 +3,48 @@
 
 # Move Classifier
 class MoveClassifier
+  def initialize
+    @enemy_color = nil
+    @diagonal_moves = nil
+    @moves_without_diagonal_moves = nil
+  end
+
   def classify_moves(color, moves, board)
-    enemy_color = color == 'white' ? 'black' : 'white'
+    create_enemy_color(color)
 
     result = { empty: [], captures: [] }
     moves.each_with_object(result) do |move, classified|
       classified[:empty] << move if board[move].empty?
-      classified[:captures] << move if board[move].piece_color == enemy_color
+      classified[:captures] << move if board[move].enemy?(@enemy_color)
     end
   end
 
   def classify_pawn_moves(moves, board, cell, color)
-    diagonal_moves = pawn_diagonal_moves(board[cell]).select { |move| moves.include?(move) }
-    remaining_moves = moves.reject { |move| diagonal_moves.include?(move) }
-
-    { empty: empty(remaining_moves, board), captures: captures(diagonal_moves, board, color) }
+    filter_moves(moves, board, cell)
+    create_enemy_color(color)
+    { empty: empty(board), captures: captures(board) }
   end
 
   private
 
+  def create_enemy_color(color)
+    @enemy_color = color == 'white' ? 'black' : 'white'
+  end
+
+  def filter_moves(moves, board, cell)
+    @diagonal_moves = pawn_diagonal_moves(board[cell]).select { |move| moves.include?(move) }
+    @moves_without_diagonal_moves = moves.reject { |move| @diagonal_moves.include?(move) }
+  end
+
   def pawn_diagonal_moves(cell)
-    [cell.top_right_diagonal, cell.top_left_diagonal, cell.bottom_right_diagonal, cell.bottom_left_diagonal].compact
+    cell.diagonals.compact
   end
 
-  def captures(diagonal_moves, board, color)
-    enemy_color = color == 'white' ? 'black' : 'white'
-
-    diagonal_moves.each_with_object([]) { |move, result| result << move if board[move].piece_color == enemy_color }
+  def captures(board)
+    @diagonal_moves.each_with_object([]) { |move, result| result << move if board[move].enemy?(@enemy_color) }
   end
 
-  def empty(moves, board)
-    moves.each_with_object([]) { |move, result| result << move if board[move].empty? }
+  def empty(board)
+    @moves_without_diagonal_moves.each_with_object([]) { |move, result| result << move if board[move].empty? }
   end
 end

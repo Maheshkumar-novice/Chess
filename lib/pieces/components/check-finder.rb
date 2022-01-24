876b2e9067a1b86d385b32534854a63f2e878281
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-24 21:04:17 +0530

    Add all cases to pieces can check

--- lib/pieces/components/check-finder.rb
+++ lib/pieces/components/check-finder.rb
@@ -22,26 +22,26 @@ class CheckFinder
   end
 
   def horizontal_check?
-    any_move_leads_to_check?(%w[r q], @move_generator.horizontal_moves(@king_position, @board))
+    any_move_leads_to_check?(%w[r q R Q], @move_generator.horizontal_moves(@king_position, @board))
   end
 
   def vertical_check?
-    any_move_leads_to_check?(%w[r q], @move_generator.vertical_moves(@king_position, @board))
+    any_move_leads_to_check?(%w[r q R Q], @move_generator.vertical_moves(@king_position, @board))
   end
 
   def diagonal_check?
-    any_move_leads_to_check?(%w[b q], @move_generator.diagonal_moves(@king_position, @board))
+    any_move_leads_to_check?(%w[b q B Q], @move_generator.diagonal_moves(@king_position, @board))
   end
 
   def knight_check?
-    any_move_leads_to_check?(%w[n], @move_generator.knight_moves(@king_position, @board))
+    any_move_leads_to_check?(%w[n N], @move_generator.knight_moves(@king_position, @board))
   end
 
   def pawn_check?
     moves = black_pawn_moves if king_cell.piece_color == 'white'
     moves = white_pawn_moves if king_cell.piece_color == 'black'
 
-    any_move_leads_to_check?(%w[p], moves)
+    any_move_leads_to_check?(%w[p P], moves)
   end
 
   def black_pawn_moves
@@ -53,7 +53,7 @@ class CheckFinder
   end
 
   def king_check?
-    any_move_leads_to_check?(%w[k], @move_generator.king_moves(@king_position, @board))
+    any_move_leads_to_check?(%w[k K], @move_generator.king_moves(@king_position, @board))
   end
 
   def any_move_leads_to_check?(pieces_can_check, moves)
@@ -73,7 +73,7 @@ class CheckFinder
   end
 
   def piece_in_move_can_check?(pieces_can_check, move)
-    pieces_can_check.include?(@board[move].piece_name.downcase)
+    pieces_can_check.include?(@board[move].piece_name)
   end
 
   def king_cell

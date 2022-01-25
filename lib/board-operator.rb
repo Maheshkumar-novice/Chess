38701984153e510eea7ff344ec34a4aef9416be2
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-25 10:11:39 +0530

    Improve board-operator readability

--- lib/board-operator.rb
+++ lib/board-operator.rb
@@ -12,15 +12,15 @@ class BoardOperator
   end
 
   def make_move(source, destination)
-    @board[destination].piece = @board[source].piece
+    @board[destination].update_piece_to(@board[source].piece)
     @board[destination].update_current_cell_of_piece(destination)
-    @board[source].piece = nil
+    @board[source].update_piece_to(nil)
   end
 
   def moves_from_source(source, color)
     @moves = @board[source].create_moves(@board)
-    reject_moves_of_same_color_destination(color)
-    reject_moves_that_keep_own_king_in_check(source, color)
+    remove_allies(color)
+    remove_moves_that_leads_to_check(source, color)
     @board[source].classify_moves(@moves, @board)
   end
 
@@ -30,37 +30,35 @@ class BoardOperator
 
   private
 
-  def reject_moves_of_same_color_destination(color)
+  def remove_allies(color)
     @moves.reject! { |move| @board[move].color?(color) }
   end
 
-  def reject_moves_that_keep_own_king_in_check(source, color)
+  def remove_moves_that_leads_to_check(source, color)
     @moves.reject! { |move| move_leads_to_check?(source, move, color) }
   end
 
   def move_leads_to_check?(source, destination, color)
-    previous_source_piece = @board[source].piece
-    previous_source_cell = previous_source_piece.current_cell
-    previous_destination_piece = @board[destination].piece
+    store_current_data(source, destination)
     make_move(source, destination)
     king_in_check = king_in_check?(color)
-    revert_move(source, destination, previous_source_piece, previous_source_cell, previous_destination_piece)
+    revert_move(source, destination)
     king_in_check
   end
 
-  def revert_move(source, destination, previous_source_piece, previous_source_cell, previous_destination_piece)
-    @board[source].piece = previous_source_piece
-    @board[destination].piece = previous_destination_piece
-    @board[source].update_current_cell_of_piece(previous_source_cell)
+  def find_king_position(king_color)
+    @board.find { |_, cell| cell.piece_color == king_color && cell.piece_name.match?(/k/i) }.first
   end
 
-  def find_king_position(king_color)
-    @board.each do |marker, cell|
-      next if cell.empty?
+  def store_current_data(source, destination)
+    @previous_source_piece = @board[source].piece
+    @previous_source_cell = @previous_source_piece.current_cell
+    @previous_destination_piece = @board[destination].piece
+  end
 
-      color = cell.piece_color
-      name = cell.piece_name
-      return marker if color == king_color && name.match?(/k/i)
-    end
+  def revert_move(source, destination)
+    @board[source].update_piece_to(@previous_source_piece)
+    @board[source].update_current_cell_of_piece(@previous_source_cell)
+    @board[destination].update_piece_to(@previous_destination_piece)
   end
 end

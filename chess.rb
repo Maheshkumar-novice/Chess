c0b3fa6488ca777de1559294b28487a6d4640651
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-25 13:26:16 +0530

    Update require order - chess

--- chess.rb
+++ chess.rb
@@ -1,8 +1,13 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-Dir['./lib/board/**/*.rb'].sort.each { |file| require file }
-Dir['./lib/chess-game/**/*.rb'].sort.each { |file| require file }
-Dir['./lib/components/**/*.rb'].sort.each { |file| require file }
-Dir['./lib/pieces/**/*.rb'].sort.each { |file| require file }
-Dir['./lib/players/**/*.rb'].sort.each { |file| require file }
+require 'forwardable'
+
+Dir['./lib/components/output/*.rb'].sort.each { |file| require file }
+Dir['./lib/components/creators/*.rb'].sort.each { |file| require file }
+Dir['./lib/components/validators/*.rb'].sort.each { |file| require file }
+Dir['./lib/components/helpers/*.rb'].sort.each { |file| require file }
+Dir['./lib/board/*.rb'].sort.each { |file| require file }
+Dir['./lib/pieces/*.rb'].sort.each { |file| require file }
+Dir['./lib/players/*.rb'].sort.each { |file| require file }
+Dir['./lib/chess-game/*.rb'].sort.each { |file| require file }

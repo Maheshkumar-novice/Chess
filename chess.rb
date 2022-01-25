90909532ff427591791a78559d0cc16838928ef9
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-25 14:03:00 +0530

    Require piece before the pieces to avoid errors

--- chess.rb
+++ chess.rb
@@ -8,6 +8,7 @@ Dir['./lib/components/creators/*.rb'].sort.each { |file| require file }
 Dir['./lib/components/validators/*.rb'].sort.each { |file| require file }
 Dir['./lib/components/helpers/*.rb'].sort.each { |file| require file }
 Dir['./lib/board/*.rb'].sort.each { |file| require file }
+Dir['./lib/pieces/piece.rb'].sort.each { |file| require file }
 Dir['./lib/pieces/*.rb'].sort.each { |file| require file }
 Dir['./lib/players/*.rb'].sort.each { |file| require file }
 Dir['./lib/chess-game/*.rb'].sort.each { |file| require file }

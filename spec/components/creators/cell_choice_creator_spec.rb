cbad23d880f8f279439f7a56e3bc150144908e9c
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-29 09:44:29 +0530

    Create new tests for cell-choice-creator

---
+++ spec/components/creators/cell_choice_creator_spec.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../../../lib/components/creators/cell-choice-creator'
+
+describe CellChoiceCreator do
+  subject(:cell_choice_creator) { described_class.new }
+
+  describe '#bot_choice' do
+    it 'returns a string' do
+      name = cell_choice_creator.bot_choice
+      expect(name).to be_a(String)
+    end
+
+    it 'returns a string of size 2' do
+      size = cell_choice_creator.bot_choice.size
+      expect(size).to eq(2)
+    end
+  end
+
+  describe '#human_choice' do
+    before do
+      allow(cell_choice_creator).to receive(:print_prompt)
+      allow(cell_choice_creator).to receive(:gets).and_return('a name')
+    end
+
+    it 'returns a string' do
+      name = cell_choice_creator.human_choice
+      expect(name).to be_a(String)
+    end
+  end
+end

53c753d4f714b7fefe5e2b07962c08a3ff4da0eb
Maheshkumar <callmeelprofessor@gmail.com>
2022-01-31 14:53:06 +0530

    Refactor result

--- lib/chess-game/result.rb
+++ lib/chess-game/result.rb
@@ -11,46 +11,55 @@ class Result
     @draw = false
     @checkmate = false
     @stalemate = false
+    @resign = false
   end
 
   def update_draw(value)
     @draw = value
   end
 
-  def any?
-    [@draw, @checkmate, @stalemate].any?
+  def update_checkmate(value)
+    @checkmate = value
   end
 
-  def update_mates(board_operator, color)
-    @checkmate = board_operator.checkmate?(color)
-    @stalemate = board_operator.stalemate?(color)
+  def update_stalemate(value)
+    @stalemate = value
+  end
+
+  def update_resign(value)
+    @resign = value
+  end
+
+  def any?
+    [@draw, @checkmate, @stalemate].any?
   end
 
   def announce(game)
     announce_draw if @draw
     announce_checkmate(game) if @checkmate
     announce_stalemate if @stalemate
+    announce_player_resignation(game) if @resign
   end
 
   def announce_draw
-    print_info("\nIt's a Draw! :(", ending: "\n")
+    print_info("It's a Draw! :(", ending: "\n", starting: "\n")
   end
 
   def announce_checkmate(game)
     current_player = game.current_player
     other_player = game.other_player
-    text = "\n#{accent(current_player.name)} Checkmated by #{accent(other_player.name)} :P"
-    print_info(text, ending: "\n")
+    text = "#{accent(current_player.name)} Checkmated by #{accent(other_player.name)} :P"
+    print_info(text, ending: "\n", starting: "\n")
   end
 
   def announce_stalemate
-    print_info("\nIt's a Stalemate! :(", ending: "\n")
+    print_info("It's a Stalemate! :(", ending: "\n", starting: "\n")
   end
 
   def announce_player_resignation(game)
     current_player = game.current_player
     other_player = game.other_player
-    text = "\n#{accent(current_player.name)} Resigned. #{accent(other_player.name)} Won!"
-    print_info(text, ending: "\n")
+    text = "#{accent(current_player.name)} Resigned. #{accent(other_player.name)} Won!"
+    print_info(text, ending: "\n", starting: "\n")
   end
 end

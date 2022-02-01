d56a965a333ecd504c0638a699e99dc66daa0065
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-01 20:39:43 +0530

    Create file-loader class

---
+++ lib/file/file-loader.rb
@@ -0,0 +1,68 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'psych'
+
+# File Loader Class
+class FileLoader
+  SAVE_DIR = 'saved_games'
+
+  def initialize
+    @files = nil
+    @files_hash = {}
+    @user_input = nil
+  end
+
+  def load
+    create_saved_games_dir unless Dir.exist?('saved_games')
+    return if saved_games_empty?
+
+    list_saves
+    load_file
+  end
+
+  def saved_games_empty?
+    Dir.empty?('saved_games')
+  end
+
+  def list_saves
+    @files = Dir.glob('saved_games/*.yml')
+    create_hash_of_files
+    print_files
+  end
+
+  def load_file
+    loop do
+      user_input
+      return yaml_load if valid_input?
+    end
+  end
+
+  def user_input
+    print 'Enter number of the file to load > '
+    @user_input = gets.chomp
+  end
+
+  def valid_input?
+    @files_hash.key?(@user_input)
+  end
+
+  def yaml_load
+    Psych.load_file(@files_hash[@user_input])
+  end
+
+  def create_saved_games_dir
+    Dir.mkdir('saved_games')
+  end
+
+  def create_hash_of_files
+    @files_hash = @files.map.with_index(1) { |file, index| [index.to_s, file] }.to_h
+  end
+
+  def print_files
+    puts 'Available Saves: '
+    @files_hash.each do |key, value|
+      puts "#{key}: #{value}"
+    end
+  end
+end

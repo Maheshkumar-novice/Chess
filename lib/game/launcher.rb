d724a70df47f17d626bfbbc7014ba447f70a8b25
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-05 17:21:19 +0530

    Add monkey patcher to launcher

--- lib/game/launcher.rb
+++ lib/game/launcher.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative '../monkey-patches/loader'
 require_relative '../display/title'
 require_relative '../display/string-color-formatter'
 require_relative '../board/board-operator'

961c111f008755256e24127f76d121f9be13d0d5
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-05 17:21:00 +0530

    Remove color.rb require from string color formatter

--- lib/display/string-color-formatter.rb
+++ lib/display/string-color-formatter.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative './color'
-
 # Methods for coloring the given string in a standard way
 module StringColorFormatter
   def print_info(str, ending: '', starting: '')

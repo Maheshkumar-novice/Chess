80ff8f503decf53fb3cbcbc6bbfbb456af279ea5
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-08 21:32:00 +0530

    Create #take_en_passant tests - piece-mover

--- spec/moves/piece_mover_spec.rb
+++ spec/moves/piece_mover_spec.rb
@@ -52,4 +52,54 @@ describe PieceMover do
       expect(board[destination].piece).to eq(previous_source_piece)
     end
   end
+
+  describe '#take_en_passant' do
+    let(:fen) { 'rnbqkbnr/p2p3p/8/2pPPp2/Pp4pP/8/1PP2PP1/RNBQKBNR w KQkq b4 0 1' }
+    let(:board) { board_creator.create_board(fen_processor.pieces) }
+    let(:meta_data) { fen_processor.meta_data }
+
+    it 'takes passant from a4' do
+      source = :b4
+      destination = :a3
+      en_passant_capture_cell = :a4
+      previous_source_piece = board[source].piece
+      piece_mover.take_en_passant(source, destination, board)
+      expect(board[source].piece).to be_nil
+      expect(board[destination].piece).to eq(previous_source_piece)
+      expect(board[en_passant_capture_cell].piece).to be_nil
+    end
+
+    it 'takes passant from c5' do
+      source = :d5
+      destination = :c6
+      en_passant_capture_cell = :c5
+      previous_source_piece = board[source].piece
+      piece_mover.take_en_passant(source, destination, board)
+      expect(board[source].piece).to be_nil
+      expect(board[destination].piece).to eq(previous_source_piece)
+      expect(board[en_passant_capture_cell].piece).to be_nil
+    end
+
+    it 'takes passant from f5' do
+      source = :e5
+      destination = :f6
+      en_passant_capture_cell = :f5
+      previous_source_piece = board[source].piece
+      piece_mover.take_en_passant(source, destination, board)
+      expect(board[source].piece).to be_nil
+      expect(board[destination].piece).to eq(previous_source_piece)
+      expect(board[en_passant_capture_cell].piece).to be_nil
+    end
+
+    it 'takes passant from h4' do
+      source = :g4
+      destination = :h3
+      en_passant_capture_cell = :h4
+      previous_source_piece = board[source].piece
+      piece_mover.take_en_passant(source, destination, board)
+      expect(board[source].piece).to be_nil
+      expect(board[destination].piece).to eq(previous_source_piece)
+      expect(board[en_passant_capture_cell].piece).to be_nil
+    end
+  end
 end

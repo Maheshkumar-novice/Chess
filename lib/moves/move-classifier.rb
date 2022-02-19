b731711d3459ffc9b763297e1e2778869d028435
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-19 18:18:58 +0530

    Update move-classifier to classify en-passant

--- lib/moves/move-classifier.rb
+++ lib/moves/move-classifier.rb
@@ -1,12 +1,15 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require_relative './special-moves'
+
 # Classifies the given moves as empty & captures
 class MoveClassifier
   def initialize
     @enemy_color = nil
     @diagonal_moves = nil
     @moves_without_diagonal_moves = nil
+    @special_moves = SpecialMoves.new
   end
 
   def classify_moves(color, moves, board)
@@ -19,10 +22,10 @@ class MoveClassifier
     end
   end
 
-  def classify_pawn_moves(moves, board, cell, color)
+  def classify_pawn_moves(moves, board, meta_data, cell, color)
     filter_moves(moves, board, cell)
     create_enemy_color(color)
-    { empty: empty(board), captures: captures(board) }
+    { empty: empty(board), captures: captures(board, meta_data, color, cell) }
   end
 
   private
@@ -44,7 +47,14 @@ class MoveClassifier
     @moves_without_diagonal_moves.each_with_object([]) { |move, result| result << move if board[move].empty? }
   end
 
-  def captures(board)
-    @diagonal_moves.each_with_object([]) { |move, result| result << move if board[move].color?(@enemy_color) }
+  def captures(board, meta_data, _color, cell)
+    @diagonal_moves.each_with_object([]) do |move, result|
+      next result << move if board[move].color?(@enemy_color)
+
+      result << move if @special_moves.en_passant?(cell,
+                                                   move,
+                                                   board,
+                                                   meta_data)
+    end
   end
 end

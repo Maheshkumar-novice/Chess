95a04a9d62b06d0edb81e94ba9be01d3a0ec564a
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-23 18:12:05 +0530

    Move monkey patch loader to core extensions

--- lib/core-extensions/loader.rb
+++ lib/core-extensions/loader.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative '../core-extensions/string/color'
+require_relative './string/color'
 
 String.include CoreExtensions::String::Color

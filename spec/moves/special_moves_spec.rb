8ca1abd9b57658fa43accab04978ec545a3a0c0c
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-25 09:43:27 +0530

    Add tests for castling - special-moves-spec

--- spec/moves/special_moves_spec.rb
+++ spec/moves/special_moves_spec.rb
@@ -160,6 +160,344 @@ describe SpecialMoves do
     end
   end
 
+  describe '#castling_move' do
+    let(:board) { board_creator.create_board(fen_processor.pieces) }
+
+    context 'when castling_rights empty' do
+      it 'returns available castlings' do
+        moves = []
+        source = :e1
+        castling_rights = ''
+        color = 'white'
+        result = special_moves.castling_move(moves, board, source, castling_rights, color)
+        expect(result).to eq([])
+      end
+    end
+
+    context 'when color don\'t have castling moves' do
+      let(:fen) { 'r1p1k1pr/b7/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+      it 'returns available castlings' do
+        moves = %i[d7 e7 f7 d8 f8]
+        source = :e8
+        castling_rights = 'KQkq'
+        color = 'black'
+        result = special_moves.castling_move(moves, board, source, castling_rights, color)
+        expect(result).to eq([])
+      end
+    end
+
+    context 'when adjacent moves not available' do
+      let(:fen) { 'r2pkp1r/b7/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+      it 'returns available castlings' do
+        moves = %i[d7 e7 f7]
+        source = :e8
+        castling_rights = 'KQkq'
+        color = 'black'
+        result = special_moves.castling_move(moves, board, source, castling_rights, color)
+        expect(result).to eq([])
+      end
+    end
+
+    context 'when cells between rook and king not empty' do
+      let(:fen) { 'rp2k1pr/b7/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+      it 'returns available castlings' do
+        moves = %i[d7 e7 f7 d8 f8]
+        source = :e8
+        castling_rights = 'KQkq'
+        color = 'black'
+        result = special_moves.castling_move(moves, board, source, castling_rights, color)
+        expect(result).to eq([])
+      end
+    end
+
+    context 'when all conditions met' do
+      let(:fen) { 'rp2k1pr/b7/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+      it 'returns available castlings' do
+        moves = %i[d1 f1 d2 e2 f2]
+        source = :e1
+        castling_rights = 'KQkq'
+        color = 'white'
+        result = special_moves.castling_move(moves, board, source, castling_rights, color)
+        expect(result).to eq(%i[g1 c1])
+      end
+    end
+  end
+
+  describe '#cells_between_king_and_rook_empty?' do
+    let(:board) { board_creator.create_board(fen_processor.pieces) }
+
+    context 'for white' do
+      context 'when moves between queen side rook and king empty' do
+        let(:fen) { 'rp2k2r/b7/8/8/8/8/8/R3K1pR b KQkq - 0 1' }
+
+        it 'returns true' do
+          source = :e1
+          castling_move = :c1
+          color = 'white'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when moves between king side rook and king empty' do
+        let(:fen) { 'rp2k2r/b7/8/8/8/8/8/R1p1K2R b KQkq - 0 1' }
+
+        it 'returns true' do
+          source = :e1
+          castling_move = :g1
+          color = 'white'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when moves between queen side rook and king not empty' do
+        let(:fen) { 'rp2k2r/b7/8/8/8/8/8/R1p1K2R b KQkq - 0 1' }
+
+        it 'returns false' do
+          source = :e1
+          castling_move = :c1
+          color = 'white'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when moves between king side rook and king notempty' do
+        let(:fen) { 'rp2k2r/b7/8/8/8/8/8/R3K1pR b KQkq - 0 1' }
+
+        it 'returns false' do
+          source = :e1
+          castling_move = :g1
+          color = 'white'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(false)
+        end
+      end
+    end
+
+    context 'for black' do
+      context 'when moves between queen side rook and king empty' do
+        let(:fen) { 'r3k1pr/8/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+        it 'returns true' do
+          source = :e8
+          castling_move = :c8
+          color = 'black'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when moves between king side rook and king empty' do
+        let(:fen) { 'rp2k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+        it 'returns true' do
+          source = :e8
+          castling_move = :g8
+          color = 'black'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(true)
+        end
+      end
+
+      context 'when moves between queen side rook and king not empty' do
+        let(:fen) { 'rp2k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+        it 'returns false' do
+          source = :e8
+          castling_move = :c8
+          color = 'black'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(false)
+        end
+      end
+
+      context 'when moves between king side rook and king not empty' do
+        let(:fen) { 'r3k1pr/8/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+
+        it 'returns false' do
+          source = :e8
+          castling_move = :g8
+          color = 'black'
+          result = special_moves.cells_between_king_and_rook_empty?(source, castling_move, board, color)
+          expect(result).to eq(false)
+        end
+      end
+    end
+  end
+
+  describe '#rook_cell' do
+    context 'for white' do
+      let(:color) { 'white' }
+
+      context 'for castling move c1' do
+        it 'returns :a1' do
+          castling_move = :c1
+          result = special_moves.rook_cell(color, castling_move)
+          expect(result).to eq(:a1)
+        end
+      end
+
+      context 'for castling move g1' do
+        it 'returns :h1' do
+          castling_move = :g1
+          result = special_moves.rook_cell(color, castling_move)
+          expect(result).to eq(:h1)
+        end
+      end
+    end
+
+    context 'for black' do
+      let(:color) { 'black' }
+
+      context 'for castling move c8' do
+        it 'returns :a8' do
+          castling_move = :c8
+          result = special_moves.rook_cell(color, castling_move)
+          expect(result).to eq(:a8)
+        end
+      end
+
+      context 'for castling move g8' do
+        it 'returns :h8' do
+          castling_move = :g8
+          result = special_moves.rook_cell(color, castling_move)
+          expect(result).to eq(:h8)
+        end
+      end
+    end
+  end
+
+  describe '#castling_has_adjacent_move?' do
+    context 'when castling has adjacent move' do
+      it 'returns true' do
+        moves = %i[a8 c8]
+        adjacent_move = :c8
+        result = special_moves.castling_has_adjacent_move?(moves, adjacent_move)
+        expect(result).to eq(true)
+      end
+    end
+
+    context 'when castling don\'t have adjacent move' do
+      it 'returns false' do
+        moves = %i[a8 c8]
+        adjacent_move = :b8
+        result = special_moves.castling_has_adjacent_move?(moves, adjacent_move)
+        expect(result).to eq(false)
+      end
+    end
+  end
+
+  describe '#adjacent_move' do
+    let(:fen) { 'rp2k2r/8/8/8/8/8/8/R3K2R b KQkq - 0 1' }
+    let(:board) { board_creator.create_board(fen_processor.pieces) }
+
+    context 'for queen side castling' do
+      it 'returns adjacent move' do
+        castling_move = :g8
+        result = special_moves.adjacent_move(castling_move, board)
+        expect(result).to eq(:f8)
+      end
+    end
+
+    context 'for king side castling' do
+      it 'returns adjacent move' do
+        castling_move = :c1
+        result = special_moves.adjacent_move(castling_move, board)
+        expect(result).to eq(:d1)
+      end
+    end
+  end
+
+  describe '#queen_side_castling?' do
+    context 'for queen side castling' do
+      it 'returns true' do
+        castling_move = :c1
+        result = special_moves.queen_side_castling?(castling_move)
+        expect(result).to eq(true)
+      end
+    end
+
+    context 'for king side castling' do
+      it 'returns false' do
+        castling_move = :g1
+        result = special_moves.queen_side_castling?(castling_move)
+        expect(result).to eq(false)
+      end
+    end
+  end
+
+  describe '#castlings_of_color' do
+    context 'for white' do
+      let(:color) { 'white' }
+
+      context 'for right KQkq' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, 'KQkq')
+          expect(result).to eq(%i[g1 c1])
+        end
+      end
+
+      context 'for right KQ' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, 'KQ')
+          expect(result).to eq(%i[g1 c1])
+        end
+      end
+
+      context 'for right kq' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, 'kq')
+          expect(result).to eq([])
+        end
+      end
+
+      context 'for empty right' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, '')
+          expect(result).to eq([])
+        end
+      end
+    end
+
+    context 'for black' do
+      let(:color) { 'black' }
+
+      context 'for right KQkq' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, 'KQkq')
+          expect(result).to eq(%i[g8 c8])
+        end
+      end
+
+      context 'for right KQ' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, 'KQ')
+          expect(result).to eq([])
+        end
+      end
+
+      context 'for right kq' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, 'kq')
+          expect(result).to eq(%i[g8 c8])
+        end
+      end
+
+      context 'for empty right' do
+        it 'returns valid rights' do
+          result = special_moves.castlings_of_color(color, '')
+          expect(result).to eq([])
+        end
+      end
+    end
+  end
+
   describe '#en_passant?' do
     let(:board) { board_creator.create_board(fen_processor.pieces) }
     let(:meta_data) { fen_processor.meta_data }

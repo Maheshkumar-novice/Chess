2dc8d82b8cb56562171b2c8fcd7f1e734a8090ad
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-28 17:33:01 +0530

    Remove initialize method - bot

--- lib/players/bot.rb
+++ lib/players/bot.rb
@@ -5,10 +5,6 @@ require_relative './player'
 
 # Represents player Bot
 class Bot < Player
-  def initialize(name_creator: NameCreator.new, cell_choice_creator: CellChoiceCreator.new)
-    super(name_creator, cell_choice_creator)
-  end
-
   def make_choice
     @cell_choice_creator.bot_choice
   end

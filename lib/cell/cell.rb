a81e4ba5d3dfc3900f50ed405672a4027da94268
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-28 17:51:18 +0530

    Refactor cell class

--- lib/cell/cell.rb
+++ lib/cell/cell.rb
@@ -10,15 +10,9 @@ class Cell
   def_delegators :piece, :create_moves, :classify_moves, :in_check?
   def_delegator :piece, :current_cell=, :update_current_cell_of_piece
 
-  attr_accessor :piece,
-                :row_right,
-                :row_left,
-                :column_above,
-                :column_below,
-                :top_left_diagonal,
-                :top_right_diagonal,
-                :bottom_right_diagonal,
-                :bottom_left_diagonal
+  attr_reader :row_right, :row_left, :column_above, :column_below,
+              :top_left_diagonal, :top_right_diagonal, :bottom_right_diagonal, :bottom_left_diagonal
+  attr_accessor :piece
 
   def create_connections(row, column)
     create_row_connections(row, column)
@@ -26,73 +20,21 @@ class Cell
     create_diagonal_connections(row, column)
   end
 
-  def diagonals
-    [top_left_diagonal, top_right_diagonal, bottom_right_diagonal, bottom_left_diagonal]
-  end
-
-  def update_piece_to(piece)
-    self.piece = piece
-  end
-
-  def piece_name
-    piece&.name
-  end
-
-  def piece_color
-    piece&.color
-  end
-
-  def color?(color)
-    piece&.color?(color)
-  end
-
-  def pawn?
-    piece_name&.downcase == 'p'
-  end
-
-  def king?
-    piece_name&.downcase == 'k'
-  end
-
-  def white_piece?
-    piece_color == 'white'
-  end
-
-  def empty?
-    piece.nil?
-  end
-
-  def occupied?
-    !empty?
-  end
-
-  def to_s
-    piece&.unicode
-  end
-
-  private
-
   def create_row_connections(row, column)
-    self.row_right = create_row_right_connection(row, column)
-    self.row_left = create_row_left_connection(row, column)
+    @row_right = create_row_right_connection(row, column)
+    @row_left = create_row_left_connection(row, column)
   end
 
   def create_column_connections(row, column)
-    self.column_above = create_column_above_connection(row, column)
-    self.column_below = create_column_below_connection(row, column)
+    @column_above = create_column_above_connection(row, column)
+    @column_below = create_column_below_connection(row, column)
   end
 
   def create_diagonal_connections(row, column)
-    self.top_left_diagonal = create_top_left_connection(row, column)
-    self.top_right_diagonal = create_top_right_connection(row, column)
-    self.bottom_right_diagonal = create_bottom_right_connection(row, column)
-    self.bottom_left_diagonal = create_bottom_left_connection(row, column)
-  end
-
-  def create_column_above_connection(row, column)
-    return if row == 8
-
-    (column + (row + 1).to_s).to_sym
+    @top_left_diagonal = create_top_left_connection(row, column)
+    @top_right_diagonal = create_top_right_connection(row, column)
+    @bottom_right_diagonal = create_bottom_right_connection(row, column)
+    @bottom_left_diagonal = create_bottom_left_connection(row, column)
   end
 
   def create_row_right_connection(row, column)
@@ -105,6 +47,12 @@ class Cell
     return if column == 'a'
 
     ((column.ord - 1).chr + row.to_s).to_sym
+  end
+
+  def create_column_above_connection(row, column)
+    return if row == 8
+
+    (column + (row + 1).to_s).to_sym
   end
 
   def create_column_below_connection(row, column)
@@ -135,5 +83,49 @@ class Cell
     return if row == 1 || column == 'a'
 
     ((column.ord - 1).chr + (row - 1).to_s).to_sym
+  end
+
+  def diagonals
+    [@top_left_diagonal, @top_right_diagonal, @bottom_right_diagonal, @bottom_left_diagonal]
+  end
+
+  def update_piece_to(piece)
+    @piece = piece
+  end
+
+  def piece_name
+    @piece&.name
+  end
+
+  def piece_color
+    @piece&.color
+  end
+
+  def color?(color)
+    @piece&.color?(color)
+  end
+
+  def pawn?
+    piece_name&.downcase == 'p'
+  end
+
+  def king?
+    piece_name&.downcase == 'k'
+  end
+
+  def white_piece?
+    piece_color == 'white'
+  end
+
+  def empty?
+    @piece.nil?
+  end
+
+  def occupied?
+    !empty?
+  end
+
+  def to_s
+    @piece&.unicode
   end
 end

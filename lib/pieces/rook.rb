6fc1d7a54c19132acff25f7a8c65e506766c95c3
Maheshkumar <callmeelprofessor@gmail.com>
2022-02-28 17:30:01 +0530

    Remove initialize method - rook

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -5,10 +5,6 @@ require_relative './piece'
 
 # Represents chess piece Rook
 class Rook < Piece
-  def initialize(move_creator: MoveCreator.new, move_classifier: MoveClassifier.new)
-    super(move_creator, move_classifier)
-  end
-
   def create_moves(board)
     @move_creator.rook_moves(@current_cell, board)
   end

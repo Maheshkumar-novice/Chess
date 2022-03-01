926d989f34adb1be6c269b0cf7a235fe7fadce76
Maheshkumar <callmeelprofessor@gmail.com>
2022-03-01 09:53:54 +0530

    Refactor piece-mover

--- lib/moves/piece-mover.rb
+++ lib/moves/piece-mover.rb
@@ -18,34 +18,36 @@ class PieceMover
 
   def regular_move(source, destination, board, meta_data)
     pieces_going_to_change = { source => board[source].piece, destination => board[destination].piece }
-    meta_data.update_changed_pieces_state(pieces_going_to_change)
 
     board[destination].update_piece_to(board[source].piece)
     board[destination].update_current_cell_of_piece(destination)
     board[source].update_piece_to(nil)
+
+    meta_data.update_changed_pieces_state(pieces_going_to_change)
   end
 
   def take_en_passant(source, destination, board, meta_data)
-    color = board[source].piece_color
-    en_passant_capture_cell = @special_moves.en_passant_capture_cell(color, destination, board)
+    en_passant_capture_cell = @special_moves.en_passant_capture_cell(board[source].piece_color, destination, board)
     pieces_going_to_change = { source => board[source].piece, destination => board[destination].piece,
                                en_passant_capture_cell => board[en_passant_capture_cell].piece }
 
     regular_move(source, destination, board, meta_data)
     board[en_passant_capture_cell].update_piece_to(nil)
+
     meta_data.update_changed_pieces_state(pieces_going_to_change)
   end
 
   def castling(source, destination, board, meta_data)
-    color = board[source].piece_color
-    rook_source = @special_moves.rook_cell(color, destination)
+    rook_source = @special_moves.rook_cell(board[source].piece_color, destination)
     rook_destination = @special_moves.adjacent_move(destination, board)
     pieces_going_to_change = { source => board[source].piece,
                                destination => board[destination].piece,
                                rook_source => board[rook_source].piece,
                                rook_destination => board[rook_destination].piece }
+
     regular_move(source, destination, board, meta_data)
     regular_move(rook_source, rook_destination, board, meta_data)
+
     meta_data.update_changed_pieces_state(pieces_going_to_change)
   end
 end

46427c924d96bf2b4496076ddf459d925b9d6541
Maheshkumar <callmeelprofessor@gmail.com>
2022-03-01 17:12:21 +0530

    Refactor moves-meta-data tests

--- spec/moves/moves_meta_data_spec.rb
+++ spec/moves/moves_meta_data_spec.rb
@@ -13,92 +13,15 @@ describe MovesMetaData do
     let(:board_creator) { BoardCreator.new }
     before { fen_processor.process(fen) }
 
-    context 'en_passant' do
-      let(:moves) { [] }
-
-      context 'when source piece is not a pawn' do
-        let(:fen) { 'rn1qkbnr/p1pp1p1p/6p1/4p3/Pb6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
-        before { moves_meta_data.instance_variable_set(:@en_passant_move, :a3) }
-
-        it 'returns a hash with en_passant as false' do
-          source = :b4
-          destination = :a3
-          result = moves_meta_data.special_moves_state(board, source, destination, moves)
-          expected_result = false
-          expect(result[:en_passant]).to eq(expected_result)
-        end
-      end
-
-      context 'when en_passant available' do
-        let(:fen) { 'rnbqkbnr/p1pp1p1p/6p1/4p3/Pp6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
-        before { moves_meta_data.instance_variable_set(:@en_passant_move, :a3) }
-
-        it 'returns a hash with en_passant as true' do
-          source = :b4
-          destination = :a3
-          result = moves_meta_data.special_moves_state(board, source, destination, moves)
-          expected_result = true
-          expect(result[:en_passant]).to eq(expected_result)
-        end
-      end
-
-      context 'when en_passant not available' do
-        let(:fen) { 'rnbqkbnr/p1pp1p1p/6p1/4p3/Pp6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
-        before { moves_meta_data.instance_variable_set(:@en_passant_move, :-) }
-
-        it 'returns a hash with en_passant as false' do
-          source = :b4
-          destination = :a3
-          result = moves_meta_data.special_moves_state(board, source, destination, moves)
-          expected_result = false
-          expect(result[:en_passant]).to eq(expected_result)
-        end
-      end
-
-      context 'when destination not matches the en_passant_move' do
-        let(:fen) { 'rnbqkbnr/p1pp1p1p/6p1/4p3/Pp6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
-        before { moves_meta_data.instance_variable_set(:@en_passant_move, :a3) }
-
-        it 'returns a hash with en_passant as false' do
-          source = :b4
-          destination = :a5
-          result = moves_meta_data.special_moves_state(board, source, destination, moves)
-          expected_result = false
-          expect(result[:en_passant]).to eq(expected_result)
-        end
-      end
-    end
-
     context 'castling' do
       let(:moves) { [] }
+      let(:board) { board_creator.create_board(fen_processor.pieces) }
 
-      context 'when source piece is not a king' do
-        let(:fen) { 'r3k2r/8/8/8/8/8/8/R3K2R w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
-        before do
-          moves_meta_data.instance_variable_set(:@castling_rights, 'KQkq')
-          allow(moves_meta_data.instance_variable_get(:@special_moves)).to receive(:castling_move).and_return([:g8])
-        end
-
-        it 'returns a hash with castling as false' do
-          source = :a1
-          destination = :a3
-          result = moves_meta_data.special_moves_state(board, source, destination, moves)
-          expected_result = false
-          expect(result[:castling]).to eq(expected_result)
-        end
-      end
-
-      context 'when castling move empty' do
+      context 'when castling move not available' do
         let(:fen) { 'rn1qkbnr/p1pp1p1p/6p1/4p3/Pb6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
         before do
           moves_meta_data.instance_variable_set(:@castling_rights, 'KQkq')
-          allow(moves_meta_data.instance_variable_get(:@special_moves)).to receive(:castling_move).and_return([])
+          allow(moves_meta_data.instance_variable_get(:@castling)).to receive(:castling?).and_return(false)
         end
 
         it 'returns a hash with castling as false' do
@@ -112,10 +35,9 @@ describe MovesMetaData do
 
       context 'when castling available' do
         let(:fen) { 'rn1qkbnr/p1pp1p1p/6p1/4p3/Pb6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
-        let(:board) { board_creator.create_board(fen_processor.pieces) }
         before do
           moves_meta_data.instance_variable_set(:@castling_rights, 'KQkq')
-          allow(moves_meta_data.instance_variable_get(:@special_moves)).to receive(:castling_move).and_return([:c1])
+          allow(moves_meta_data.instance_variable_get(:@castling)).to receive(:castling?).and_return(true)
         end
 
         it 'returns a hash with castling as true' do
@@ -126,6 +48,68 @@ describe MovesMetaData do
           expect(result[:castling]).to eq(expected_result)
         end
       end
+    end
+
+    context 'en_passant' do
+      let(:moves) { [] }
+      let(:board) { board_creator.create_board(fen_processor.pieces) }
+
+      context 'when en_passant available' do
+        let(:fen) { 'rnbqkbnr/p1pp1p1p/6p1/4p3/Pp6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
+        before do
+          moves_meta_data.instance_variable_set(:@en_passant_move, :a3)
+          allow(moves_meta_data.instance_variable_get(:@en_passant)).to receive(:en_passant?).and_return(true)
+        end
+
+        it 'returns a hash with en_passant as true' do
+          source = :b4
+          destination = :a3
+          result = moves_meta_data.special_moves_state(board, source, destination, moves)
+          expected_result = true
+          expect(result[:en_passant]).to eq(expected_result)
+        end
+      end
+
+      context 'when en_passant not available' do
+        let(:fen) { 'rnbqkbnr/p1pp1p1p/6p1/4p3/Pp6/1P6/2P1PPPP/RNBQKBNR w KQkq - 0 1' }
+        before do
+          moves_meta_data.instance_variable_set(:@en_passant_move, :-)
+          allow(moves_meta_data.instance_variable_get(:@en_passant)).to receive(:en_passant?).and_return(false)
+        end
+
+        it 'returns a hash with en_passant as false' do
+          source = :b4
+          destination = :a3
+          result = moves_meta_data.special_moves_state(board, source, destination, moves)
+          expected_result = false
+          expect(result[:en_passant]).to eq(expected_result)
+        end
+      end
+    end
+  end
+
+  describe '#update' do
+    before do
+      allow(moves_meta_data.instance_variable_get(:@castling)).to receive(:update_castling_rights).and_return('castling')
+      allow(moves_meta_data.instance_variable_get(:@en_passant)).to receive(:create_en_passant_move).and_return('en_passant')
+    end
+
+    it 'updates castling_rights' do
+      source = :a1
+      destination = :a2
+      board = {}
+      moves_meta_data.update(source, destination, board)
+      castling_rights = moves_meta_data.instance_variable_get(:@castling_rights)
+      expect(castling_rights).to eq('castling')
+    end
+
+    it 'update en_passant_move' do
+      source = :a1
+      destination = :a2
+      board = {}
+      moves_meta_data.update(source, destination, board)
+      en_passant_move = moves_meta_data.instance_variable_get(:@en_passant_move)
+      expect(en_passant_move).to eq('en_passant')
     end
   end
 end

807e65228124e50f36b46a7d5f9c14f10fc8c161
Maheshkumar <callmeelprofessor@gmail.com>
2022-03-01 15:07:07 +0530

    Refactor fen-processor-spec

--- spec/fen/fen_processor_spec.rb
+++ spec/fen/fen_processor_spec.rb
@@ -4,8 +4,7 @@
 require_relative '../../lib/fen/fen-processor'
 
 describe FenProcessor do
-  subject(:fen_processor) { described_class.new(piece_creator: piece_creator) }
-  let(:piece_creator) { double('PieceCreator') }
+  subject(:fen_processor) { described_class.new }
   let(:fen) { 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' }
 
   before do
@@ -16,11 +15,6 @@ describe FenProcessor do
   describe '#process' do
     before { fen_processor.process(fen) }
 
-    it 'creates pieces' do
-      pieces = fen_processor.instance_variable_get(:@pieces)
-      expect(pieces).not_to be_nil
-    end
-
     it 'creates 64 pieces' do
       pieces = fen_processor.instance_variable_get(:@pieces)
       expect(pieces.size).to eq(64)
@@ -28,54 +22,22 @@ describe FenProcessor do
 
     it 'sets current color' do
       current_color = fen_processor.instance_variable_get(:@current_color)
-      expect(current_color).not_to be_nil
-    end
-  end
-
-  describe '#split' do
-    it 'splits the given fen into 8 parts' do
-      result = fen_processor.split(fen).size
-      expect(result).to eq(8)
-    end
-  end
-
-  describe '#to_pieces' do
-    let(:split) { fen_processor.split(fen) }
-
-    it 'converts rows into 64 pieces' do
-      result = fen_processor.to_pieces(split).size
-      expect(result).to eq(64)
-    end
-  end
-
-  describe '#parse_color' do
-    context 'when the value is w' do
-      it 'returns white' do
-        result = fen_processor.parse_color('w')
-        expect(result).to eq('white')
-      end
-    end
-
-    context 'when the value is b' do
-      it 'returns black' do
-        result = fen_processor.parse_color('b')
-        expect(result).to eq('black')
-      end
+      expect(current_color).to eq('white')
     end
   end
 
   describe '#parse_remaining_meta_data' do
-    let(:meta_data) { fen_processor.split(fen)[-1].split[1..] }
+    let(:meta_data) { %w[a b] }
 
-    it 'sends :en_passant= message to meta_data' do
+    it 'sends :update_castling_rights_to message to meta_data' do
       meta_data_obj = fen_processor.instance_variable_get(:@meta_data)
-      expect(meta_data_obj).to receive(:en_passant_move=).with(meta_data[1].to_sym)
+      expect(meta_data_obj).to receive(:update_castling_rights_to).with(meta_data[0])
       fen_processor.parse_remaining_meta_data(meta_data)
     end
 
-    it 'sends :castling_rights= message to meta_data' do
+    it 'sends :update_en_passant_move_to message to meta_data' do
       meta_data_obj = fen_processor.instance_variable_get(:@meta_data)
-      expect(meta_data_obj).to receive(:castling_rights=).with(meta_data[0])
+      expect(meta_data_obj).to receive(:update_en_passant_move_to).with(meta_data[1])
       fen_processor.parse_remaining_meta_data(meta_data)
     end
   end
